52e6096442c04de55c3edaa6f521d45f9851342e
Owen Reynolds <50507806+getvpro@users.noreply.github.com>
2025-10-02 09:53:36 -0400

    Runs only against VMXNET3 type NICS
    Reference blog: https://docs.jeffriechers.com/Microsoft/24h2-windows-11-and-server-2025-vmxnet3-issues/

--- Optimize-VMXNET3.ps1
+++ Optimize-VMXNET3.ps1
@@ -1,16 +1,41 @@
-﻿Disable-NetAdapterChecksumOffload -Name "*" -IncludeHidden -IpIPV4 -TcpIPV4 -TcpIPV6 -UdpIPV4 -UdpIPV6 -NoRestart
-Disable-NetAdapterLso -Name "*" -IncludeHidden -IPv4 -IPv6 -NoRestart
-Disable-NetAdapterEncapsulatedPacketTaskOffload -Name "*" -IncludeHidden -NoRestart
-Disable-NetAdapterIPsecOffload -Name "*" -IncludeHidden -NoRestart
-Disable-NetAdapterRss -Name "*" -IncludeHidden -NoRestart
-Disable-NetAdapterRsc -Name "*" -IncludeHidden -IPv4 -IPv6 -NoRestart
-Disable-NetAdapterPowerManagement -Name "*" -NoRestart
-Disable-NetAdapterUso -Name "*" -IncludeHidden -IPv4 -IPv6 -NoRestart
-Set-NetAdapterAdvancedProperty -Name "*" -IncludeHidden -DisplayName "Packet Priority & VLAN" -DisplayValue "Packet Priority & VLAN Disabled" -NoRestart
-Set-NetAdapterAdvancedProperty -Name "*" -IncludeHidden -DisplayName "Priority / VLAN tag" -DisplayValue "Priority & VLAN Disabled" -NoRestart
-Set-NetAdapterAdvancedProperty -Name "*" -IncludeHidden -DisplayName "Interrupt Moderation" -DisplayValue "Disabled" -NoRestart
-Set-NetAdapterAdvancedProperty -Name "*" -IncludeHidden -DisplayName "Offload IP Options" -DisplayValue "Disabled" -NoRestart
-Set-NetAdapterAdvancedProperty -Name "*" -IncludeHidden -DisplayName "Offload TCP Options" -DisplayValue "Disabled" -NoRestart
-Set-NetAdapterAdvancedProperty -Name "*" -IncludeHidden -DisplayName "Offload tagged traffic" -DisplayValue "Disabled" -NoRestart
-Set-NetAdapterAdvancedProperty -Name "*" -IncludeHidden -DisplayName "Interrupt Coalescing Scheme Mode" -DisplayValue "Disabled" -NoRestart
-Disable-NetAdapterPowerManagement -Name "*" -IncludeHidden  -D0PacketCoalescing -DeviceSleepOnDisconnect -NSOffload -SelectiveSuspend -RsnRekeyOffload
+﻿$vmxnet3Adapters = Get-NetAdapter -IncludeHidden | Where-Object { $_.InterfaceDescription -like '*VMXNET3*' }
+
+foreach ($adapter in $vmxnet3Adapters) {
+    $name = $adapter.Name
+
+    Write-Host "Disabling Checksum Offload on adapter $name"
+    Disable-NetAdapterChecksumOffload -Name $name -IpIPV4 -TcpIPV4 -TcpIPV6 -UdpIPV4 -UdpIPV6 -NoRestart
+
+    Write-Host "Disabling LSO on adapter $name"
+    Disable-NetAdapterLso -Name $name -IPv4 -IPv6 -NoRestart
+
+    Write-Host "Disabling Encapsulated Packet Task Offload on adapter $name"
+    Disable-NetAdapterEncapsulatedPacketTaskOffload -Name $name -NoRestart
+
+    Write-Host "Disabling IPsec Offload on adapter $name"
+    Disable-NetAdapterIPsecOffload -Name $name -NoRestart
+
+    Write-Host "Disabling RSS on adapter $name"
+    Disable-NetAdapterRss -Name $name -NoRestart
+
+    Write-Host "Disabling RSC on adapter $name"
+    Disable-NetAdapterRsc -Name $name -IPv4 -IPv6 -NoRestart
+
+    Write-Host "Disabling Power Management on adapter $name"
+    Disable-NetAdapterPowerManagement -Name $name -NoRestart
+
+    Write-Host "Disabling USO on adapter $name"
+    Disable-NetAdapterUso -Name $name -IPv4 -IPv6 -NoRestart
+
+    Write-Host "Setting advanced properties on adapter $name"
+    Set-NetAdapterAdvancedProperty -Name $name -DisplayName "Packet Priority & VLAN" -DisplayValue "Packet Priority & VLAN Disabled" -NoRestart
+    Set-NetAdapterAdvancedProperty -Name $name -DisplayName "Priority / VLAN tag" -DisplayValue "Priority & VLAN Disabled" -NoRestart
+    Set-NetAdapterAdvancedProperty -Name $name -DisplayName "Interrupt Moderation" -DisplayValue "Disabled" -NoRestart
+    Set-NetAdapterAdvancedProperty -Name $name -DisplayName "Offload IP Options" -DisplayValue "Disabled" -NoRestart
+    Set-NetAdapterAdvancedProperty -Name $name -DisplayName "Offload TCP Options" -DisplayValue "Disabled" -NoRestart
+    Set-NetAdapterAdvancedProperty -Name $name -DisplayName "Offload tagged traffic" -DisplayValue "Disabled" -NoRestart
+    Set-NetAdapterAdvancedProperty -Name $name -DisplayName "Interrupt Coalescing Scheme Mode" -DisplayValue "Disabled" -NoRestart
+
+    Write-Host "Disabling advanced Power Management features on adapter $name"
+    Disable-NetAdapterPowerManagement -Name $name -D0PacketCoalescing -DeviceSleepOnDisconnect -NSOffload -SelectiveSuspend -RsnRekeyOffload -NoRestart
+}
